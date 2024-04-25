a3d198ba83f16a649de0b21888c88fe388a3ecd2
Jayden Liaw <liawjianwei@outlook.com>
2024-04-25 18:57:20 +0800

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import GitLogo from "../assets/github.svg";
 import Linkedin from "../assets/linkedin.svg";
 import ReactIcon from "../assets/react.svg";
